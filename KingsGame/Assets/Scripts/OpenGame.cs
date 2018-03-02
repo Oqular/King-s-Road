58ff22712acf5de7332d0b3225eb338c92c08903
Fox <edvardas.taip@gmail.com>
2018-03-02 22:01:24 +0200

    Menu
    yay me

---
+++ KingsGame/Assets/Scripts/OpenGame.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGame : MonoBehaviour {
+    public void OpenGameScene()
+    {
+        Application.LoadLevel(0);
+    }
+}
